4f8336d59d4d67c08143fac56703694df88d9bf2
Dylan Balagtas <dylansatgalab@gmail.com>
2023-12-23 22:53:44 +0800

    hero page with grid alignment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import './App.css';
+
 function App() {
   return (
     <div className="App">
+      <nav>
+        <div></div>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Blog</a></li>
+        </ul>
+        <div>
+          <button>Contact Me</button>
+        </div>
+      </nav>
+      <div className='Hero'>
+        <div>
+          <div>
+        <h3>Hi I'm, Dylan</h3>
+        <h1>Web Developer</h1>
+        <h2>Transforming Complex Web Challenges <br/>
+into Elegant, Simple Solutions</h2>
+          </div>
+        <button>Let's Connect</button>
+        </div>
+        <div>image goes here</div>
+      </div>
     </div>
   );
 }
