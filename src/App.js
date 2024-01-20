f37c8b37c2fd6fe83d3edcc6b6662a3095935b13
Dylan Balagtas <dylansatgalab@gmail.com>
2024-01-20 23:18:02 +0800

    style link  and updates url

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <ul>
           <li><a href="#">Home</a></li>
           <li><a href="#">Projects</a></li>
-          <li><a href="#">Blog</a></li>
+          <li><a href="https://dev.to/dylan_balagtas">Blog</a></li>
         </ul>
         <div>
           <button>Contact Me</button>
